75a31416d3f7410b60032780d36bedcf3a67bb7c
Jenn Crockett <jmcrockett2@gmail.com>
2018-08-12 11:08:28 -0500

    while loops

---
+++ src/CountTo30.java
@@ -0,0 +1,16 @@
+import static java.lang.System.out;
+
+public class CountTo30
+{
+    public static void main(String[] args)
+    {
+        int number = 1;
+        System.out.println(number);
+
+        while (number < 30)
+        {
+            number++;
+            System.out.println(number);
+        }
+    }
+}
