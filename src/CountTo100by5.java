3dc9a90e8806a5557bda79915e50b69b560cc71f
Jenn Crockett <jmcrockett2@gmail.com>
2018-08-12 11:12:28 -0500

    while loops

---
+++ src/CountTo100by5.java
@@ -0,0 +1,16 @@
+import static java.lang.System.out;
+
+public class CountTo100by5
+{
+    public static void main (String[] args)
+    {
+        int number = 0;
+        System.out.println(number);
+
+        while(number < 100)
+        {
+            number = number + 5;
+            System.out.println(number);
+        }
+    }
+}
